b6de6ac0928d73a9156baebf85623da0c247b91f
andrew-propelauth <89474256+andrew-propelauth@users.noreply.github.com>
2025-02-10 14:56:28 -0800

    Added UserInOrgMetadata

--- src/server/index.ts
+++ src/server/index.ts
@@ -55,6 +55,7 @@ export type {
     UpdateUserPasswordException,
     UpdateUserPasswordRequest,
     UserNotFoundException,
+    UserInOrgMetadata,
     UsersInOrgQuery,
     UsersPagedResponse,
     UsersInOrgPagedResponse,
