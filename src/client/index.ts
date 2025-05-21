fbfb8162b74d05809d1ae88c6a9cdba8b7d0cac2
James Barker <jamesbarkr@gmail.com>
2025-05-21 09:42:48 -0700

    feat: added nextjs support for ui.propelauth.com (#74)
    * feat: added nextjs support for ui.propelauth.com
    
    * chore: action PR comments
    
    * chore: forwarded response status
    
    * fix: just pass through the body
    
    * chore: actually just pass through the whole response lol
    
    * chore: add descriptive error message to help with people setting things up wrong

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,11 +1,13 @@
-export type {OrgIdToOrgMemberInfo} from "../user"
-export {UserFromToken, OrgMemberInfo} from "../user"
-export {AuthProvider} from "./AuthProvider"
-export type {AuthProviderProps, RedirectToLoginOptions, RedirectToSignupOptions} from "./AuthProvider"
-export {useUser, User} from "./useUser"
-export type {UseUser, UseUserLoading, UseUserLoggedIn, UseUserNotLoggedIn} from "./useUser"
-export {useHostedPageUrls} from "./useHostedPageUrls"
-export {useLogoutFunction} from "./useLogoutFunction"
-export {useRedirectFunctions, RedirectToLogin, RedirectToSignup} from "./useRedirectFunctions"
-export type {RedirectProps} from "./useRedirectFunctions"
-export {useRefreshAuth} from "./useRefreshAuth"
+export type { OrgIdToOrgMemberInfo } from '../user'
+export { UserFromToken, OrgMemberInfo } from '../user'
+export { AuthProvider } from './AuthProvider'
+export type { AuthProviderProps, RedirectToLoginOptions, RedirectToSignupOptions } from './AuthProvider'
+export { useUser, User } from './useUser'
+export type { UseUser, UseUserLoading, UseUserLoggedIn, UseUserNotLoggedIn } from './useUser'
+export { useHostedPageUrls } from './useHostedPageUrls'
+export { useLogoutFunction } from './useLogoutFunction'
+export { useRedirectFunctions, RedirectToLogin, RedirectToSignup } from './useRedirectFunctions'
+export type { RedirectProps } from './useRedirectFunctions'
+export { useRefreshAuth } from './useRefreshAuth'
+export { useAuthUrl } from './useAuthUrl'
+
